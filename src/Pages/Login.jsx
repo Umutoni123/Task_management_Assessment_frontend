b3ef97aa95efa3c83ca317c1bd8ca6defba9b947
noella <noellaumutoni5@gmail.com>
2023-11-29 10:04:49 +0200

    Your commit message

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -24,9 +24,9 @@ export default function Login() {
   return (
     <div>
       
-      <div className='w-full md:w-[1540px] mx-auto mt-[80px] md:mt-[122px] px-4 md:px-0 justify-between'>
+      <div className='w-full md:w-[96.25rem] mx-auto mt-[80px] md:mt-[122px] px-4 md:px-0 justify-between'>
                 <div className='flex flex-col w-[500px] mx-auto'>
-                    <h1 className='w-full text-[96.25rem] text-[#565656] font-bold text-center'>Hello <span className='text-teal-500 font-nunito tracking-[0.02em] leading-[70.75px]'>Again</span></h1>
+                    <h1 className='w-full text-[54px] text-[#565656] font-bold text-center'>Hello <span className='text-teal-500 font-nunito tracking-[0.02em] leading-[70.75px]'>Again</span></h1>
                     <p className='text-[23.19px] font-normal font-nunito leading-[32.44px] tracking-[0.02em] text-[#7e7f88] text-center'>Welcome back you've been missed!</p>
                     <input value={data.phone} onChange={(e) => setData({ ...data, phone: e.target.value })} className='w-full border h-[72px] bg-white rounded-[20px] px-[20px] font-nunito text-[20px] leading-[0.02em] placeholder:text-[#9092B0] font-normal mt-[50px]' placeholder='Enter phone number' type='text' />
                     <input value={data.password} onChange={(e) => setData({ ...data, password: e.target.value })} className='w-full border h-[72px] bg-white rounded-[20px] px-[20px] font-nunito text-[20px] leading-[0.02em] placeholder:text-[#9092B0] font-normal mt-[20px] mb-[20px]' placeholder='Password' type='password' />
